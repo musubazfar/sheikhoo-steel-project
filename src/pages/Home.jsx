abf139f99a35b23fddb6b7b9967e1e501c39a83f
Musub <musab.azfar@lhr.ppic3.gop.pk>
2024-10-03 17:56:57 +0500

    updated coede

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, useMediaQuery } from '@mui/material'
 import homeBanner from '../assets/sheikhoo-steel-video-banner.png'
 import '../styles/Home.css'
 import Timeline from '../components/Timeline'
+import { useTheme } from '@emotion/react'
 
 const Home = () => {
+    const theme = useTheme();
+    const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
+    console.log(isMdUp)
   return (
     <>
     <Box className='banner-container'>
@@ -12,7 +16,7 @@ const Home = () => {
     </Box>
     <Timeline/>
     <Box className='Steel_game_change'>
-        <Typography variant='h1' textAlign={'center'} fontWeight={800} color='white'>HOW OUR STEEL <br/> CHANGES THE GAME</Typography>
+        <Typography variant={'h1'} textAlign={'center'} fontWeight={600} fontSize={isMdUp ? '5rem' : '3rem'} color='white'>HOW OUR STEEL <br/> CHANGES THE GAME</Typography>
     </Box>
     </>
   )
